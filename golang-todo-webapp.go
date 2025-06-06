b363d5d20c4d4ebbab5afe15a27b85e10695ee7b
Chris Couzens <ccouzens@gmail.com>
2025-06-06 13:38:30 +0100

    Create a variable for the application state
    I'm dissapointed go fmt didn't format the struct literal and I had to
    do it myself.

--- golang-todo-webapp.go
+++ golang-todo-webapp.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+type App struct {
+	Todos []Todo
+}
+
+type Todo struct {
+	Id        string
+	Text      string
+	Completed bool
+}
+
 func main() {
-	fmt.Println("Hello, world.")
+	var app = App{Todos: []Todo{
+		{Id: "265f5f64-42d2-11f0-b515-5acc179b5fff", Text: "Get to the airport", Completed: false},
+		{Id: "8b63cc6a-42d2-11f0-80b2-5acc179b5fff", Text: "Buy lunch", Completed: true},
+		{Id: "f5cbe95c-42d2-11f0-85e3-5acc179b5fff", Text: "Pack European travel adapter", Completed: false},
+	}}
+	fmt.Println(app)
 }
