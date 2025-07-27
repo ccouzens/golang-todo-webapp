93449b60c2642873402c0fba85066fff2bb5308f
Chris Couzens <ccouzens@gmail.com>
2025-07-27 11:29:27 +0100

    Finish setting up biome

--- vite_version/todo-mvc-ssr/src/main.tsx
+++ vite_version/todo-mvc-ssr/src/main.tsx
@@ -2,4 +2,10 @@ import { render } from "preact";
 import "./index.css";
 import { App } from "./app.tsx";
 
-render(<App />, document.getElementById("app")!);
+const appElement = document.getElementById("app");
+
+if (appElement === null) {
+	throw new Error("App element couldn't be found");
+}
+
+render(<App />, appElement);
