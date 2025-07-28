726bf59fb9870e5a9c143fec6064b79b5eff0a87
Chris Couzens <ccouzens@gmail.com>
2025-07-28 07:29:41 +0100

    Plugin the URL parsing
    http://localhost:5173/?todos[]text=get+haircut&todos[]isCompleted=true&todos[]text=mop+the+floor&todos[]isCompleted=false

--- vite_version/todo-mvc-ssr/src/client.tsx
+++ vite_version/todo-mvc-ssr/src/client.tsx
@@ -1,6 +1,7 @@
 import { render } from "preact";
 import "./index.css";
-import { App, type Todo } from "./app.tsx";
+import { App } from "./app.tsx";
+import { parseTodosFromUrlSearchParams } from "./parse-todos-from-url-search-params.ts";
 
 const appElement = document.getElementById("app");
 
@@ -8,6 +9,8 @@ if (appElement === null) {
 	throw new Error("App element couldn't be found");
 }
 
-const todos: Todo[] = [];
+const todos = parseTodosFromUrlSearchParams(
+	new URLSearchParams(window.location.search),
+);
 
 render(<App todos={todos} />, appElement);
