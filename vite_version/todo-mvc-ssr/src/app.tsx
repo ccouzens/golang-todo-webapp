8cad956afc3d317b2667916063b46b1f36c0b076
Chris Couzens <ccouzens@gmail.com>
2025-08-02 06:01:26 +0100

    Continue building out TodoMVC MPA

--- vite_version/todo-mvc-ssr/src/app.tsx
+++ vite_version/todo-mvc-ssr/src/app.tsx
@@ -1,4 +1,3 @@
-import "./app.css";
 import type { FunctionComponent } from "preact";
 
 export type Todo = {
@@ -11,7 +10,14 @@ export const App: FunctionComponent<{
 }> = ({ todos }) => {
 	return (
 		<>
+			<form id="newTodo" method="get"></form>
 			<h1>todos</h1>
+			<input
+				form="newTodo"
+				type="text"
+				placeholder="What needs to be done?"
+				name="todos[]text"
+			/>
 			{todos.map((todo) => {
 				return <div>{todo.text}</div>;
 			})}
