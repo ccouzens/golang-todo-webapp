430e2ef4a08c169a687e54d234b92b93f0bd89a0
Chris Couzens <ccouzens@gmail.com>
2025-08-04 13:53:28 +0100

    Create a server side rendered dev mode

---
+++ vite_version/todo-mvc-ssr/src/server.tsx
@@ -0,0 +1,10 @@
+import { renderToString } from "preact-render-to-string";
+import "./index.css";
+import { App } from "./app.tsx";
+import { parseTodosFromUrlSearchParams } from "./parse-todos-from-url-search-params.ts";
+
+export function render(url: string): string {
+	const queryParams = new URL(`https://a${url}`).searchParams;
+	const todos = parseTodosFromUrlSearchParams(queryParams);
+	return renderToString(<App todos={todos} />);
+}
